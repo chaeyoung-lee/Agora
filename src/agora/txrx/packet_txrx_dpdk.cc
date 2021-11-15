46e66f38dc3f891292d20ce5f35374c1641fa987
Andrew Sedlmayr <andrew.sedlmayr@rice.edu>
2021-11-15 10:21:00 -0600

    Transferring to test dpdk mode

--- src/agora/txrx/packet_txrx_dpdk.cc
+++ src/agora/txrx/packet_txrx_dpdk.cc
@@ -69,9 +69,7 @@ PacketTxRxDpdk::PacketTxRxDpdk(
 
 PacketTxRxDpdk::~PacketTxRxDpdk() { rte_mempool_free(mbuf_pool_); }
 
-bool PacketTxRxDpdk::StartTxRx(Table<char>& buffer, size_t packet_num_in_buffer,
-                               Table<size_t>& frame_start, char* tx_buffer,
-                               Table<complex_float>& calib_dl_buffer,
+bool PacketTxRxDpdk::StartTxRx(Table<complex_float>& calib_dl_buffer,
                                Table<complex_float>& calib_ul_buffer) {
   unused(calib_dl_buffer);
   unused(calib_ul_buffer);
@@ -85,7 +83,7 @@ bool PacketTxRxDpdk::StartTxRx(Table<char>& buffer, size_t packet_num_in_buffer,
     rx_packets_.at(i).reserve(buffers_per_socket_);
 #if defined(USE_DPDK_MEMORY)
     unused(buffer);
-    rx_packets_.at(i).resize(buffers_per_socket_);
+    rx_packets_dpdk_.at(i).resize(buffers_per_socket_);
 #else
     for (size_t number_packets = 0; number_packets < buffers_per_socket_;
          number_packets++) {
@@ -125,14 +123,15 @@ void PacketTxRxDpdk::DoTxRx(size_t tid) {
   const uint16_t queue_id = tid / cfg_->DpdkNumPorts();
 
   while (cfg_->Running()) {
-    if (-1 == DequeueSend(tid)) {
+    if (0 == DequeueSend(tid)) {
       DpdkRecv((int)tid, port_id, queue_id, prev_frame_id, rx_slot);
     }
   }
 }
 
-uint16_t PacketTxRxDpdk::DpdkRecv(int tid, uint16_t port_id, uint16_t queue_id,
-                                  size_t& prev_frame_id, size_t& rx_slot) {
+uint16_t PacketTxRxDpdk::DpdkRecv(size_t tid, uint16_t port_id,
+                                  uint16_t queue_id, size_t& prev_frame_id,
+                                  size_t& rx_slot) {
   rte_mbuf* rx_bufs[kRxBatchSize];
   uint16_t nb_rx = rte_eth_rx_burst(port_id, queue_id, rx_bufs, kRxBatchSize);
   if (unlikely(nb_rx == 0)) return 0;
@@ -216,68 +215,79 @@ uint16_t PacketTxRxDpdk::DpdkRecv(int tid, uint16_t port_id, uint16_t queue_id,
 }
 
 // TODO: check correctness of this funcion
-int PacketTxRxDpdk::DequeueSend(int tid) {
+size_t PacketTxRxDpdk::DequeueSend(size_t tid) {
   EventData event;
-  if (task_queue_->try_dequeue_from_producer(*tx_ptoks_[tid], event) == false) {
-    return -1;
+  size_t dequeue_events = 0;
+
+  bool status = task_queue_->try_dequeue_from_producer(*tx_ptoks_[tid], event);
+  if (status == true) {
+    dequeue_events = 1;
+    // std::printf("tx queue length: %d\n", task_queue_->size_approx());
+    assert(event.event_type_ == EventType::kPacketTX);
+
+    size_t ant_id = gen_tag_t(event.tags_[0]).ant_id_;
+    size_t frame_id = gen_tag_t(event.tags_[0]).frame_id_;
+    size_t symbol_id = gen_tag_t(event.tags_[0]).symbol_id_;
+
+    size_t data_symbol_idx_dl = this->cfg_->Frame().GetDLSymbolIdx(symbol_id);
+    size_t offset =
+        (this->cfg_->GetTotalDataSymbolIdxDl(frame_id, data_symbol_idx_dl) *
+         this->cfg_->BsAntNum()) +
+        ant_id;
+
+    if (kDebugPrintInTask) {
+      std::printf(
+          "In TX thread %d: Transmitted frame %zu, symbol %zu, "
+          "ant %zu, tag %zu, offset: %zu, msg_queue_length: %zu\n",
+          tid, frame_id, symbol_id, ant_id, gen_tag_t(event.tags_[0]).tag_,
+          offset, message_queue_->size_approx());
+    }
+
+    char* cur_buffer_ptr = tx_buffer_ + offset * this->cfg_->DlPacketLength();
+    auto* pkt = (Packet*)cur_buffer_ptr;
+    new (pkt) Packet(frame_id, symbol_id, 0 /* cell_id */, ant_id);
+
+    struct rte_mbuf* tx_bufs[kTxBatchSize] __attribute__((aligned(64)));
+    tx_bufs[0] = rte_pktmbuf_alloc(mbuf_pool_);
+    struct rte_ether_hdr* eth_hdr =
+        rte_pktmbuf_mtod(tx_bufs[0], struct rte_ether_hdr*);
+    eth_hdr->ether_type = rte_be_to_cpu_16(RTE_ETHER_TYPE_IPV4);
+
+    struct rte_ipv4_hdr* ip_h =
+        (struct rte_ipv4_hdr*)((char*)eth_hdr + sizeof(struct rte_ether_hdr));
+    ip_h->src_addr = bs_server_addr_;
+    ip_h->dst_addr = bs_rru_addr_;
+    ip_h->next_proto_id = IPPROTO_UDP;
+
+    struct rte_udp_hdr* udp_h =
+        (struct rte_udp_hdr*)((char*)ip_h + sizeof(struct rte_ipv4_hdr));
+    udp_h->src_port = rte_cpu_to_be_16(this->cfg_->BsServerPort() + tid);
+    udp_h->dst_port = rte_cpu_to_be_16(this->cfg_->BsRruPort() + tid);
+
+    tx_bufs[0]->pkt_len = this->cfg_->DlPacketLength() + kPayloadOffset;
+    tx_bufs[0]->data_len = this->cfg_->DlPacketLength() + kPayloadOffset;
+    char* payload = (char*)eth_hdr + kPayloadOffset;
+    DpdkTransport::FastMemcpy(payload, (char*)pkt,
+                              this->cfg_->DlPacketLength());
+
+    // Send data (one OFDM symbol)
+    size_t nb_tx_new = rte_eth_tx_burst(0, tid, tx_bufs, 1);
+    if (unlikely(nb_tx_new != 1)) {
+      std::printf("rte_eth_tx_burst() failed\n");
+      throw std::runtime_error("PacketTxRxDpdk: rte_eth_tx_burst() failed");
+    }
+    RtAssert(
+        message_queue_->enqueue(
+            *rx_ptoks_[tid], EventData(EventType::kPacketTX, event.tags_[0])),
+        "Socket message enqueue failed\n");
   }
+  return dequeue_events;
+}
 
-  // std::printf("tx queue length: %d\n", task_queue_->size_approx());
-  assert(event.event_type_ == EventType::kPacketTX);
-
-  size_t ant_id = gen_tag_t(event.tags_[0]).ant_id_;
-  size_t frame_id = gen_tag_t(event.tags_[0]).frame_id_;
-  size_t symbol_id = gen_tag_t(event.tags_[0]).symbol_id_;
-
-  size_t data_symbol_idx_dl = this->cfg_->Frame().GetDLSymbolIdx(symbol_id);
-  size_t offset =
-      (this->cfg_->GetTotalDataSymbolIdxDl(frame_id, data_symbol_idx_dl) *
-       this->cfg_->BsAntNum()) +
-      ant_id;
-
-  if (kDebugPrintInTask) {
-    std::printf(
-        "In TX thread %d: Transmitted frame %zu, symbol %zu, "
-        "ant %zu, tag %zu, offset: %zu, msg_queue_length: %zu\n",
-        tid, frame_id, symbol_id, ant_id, gen_tag_t(event.tags_[0]).tag_,
-        offset, message_queue_->size_approx());
-  }
-
-  char* cur_buffer_ptr = tx_buffer_ + offset * this->cfg_->DlPacketLength();
-  auto* pkt = (Packet*)cur_buffer_ptr;
-  new (pkt) Packet(frame_id, symbol_id, 0 /* cell_id */, ant_id);
-
-  struct rte_mbuf* tx_bufs[kTxBatchSize] __attribute__((aligned(64)));
-  tx_bufs[0] = rte_pktmbuf_alloc(mbuf_pool_);
-  struct rte_ether_hdr* eth_hdr =
-      rte_pktmbuf_mtod(tx_bufs[0], struct rte_ether_hdr*);
-  eth_hdr->ether_type = rte_be_to_cpu_16(RTE_ETHER_TYPE_IPV4);
-
-  struct rte_ipv4_hdr* ip_h =
-      (struct rte_ipv4_hdr*)((char*)eth_hdr + sizeof(struct rte_ether_hdr));
-  ip_h->src_addr = bs_server_addr_;
-  ip_h->dst_addr = bs_rru_addr_;
-  ip_h->next_proto_id = IPPROTO_UDP;
-
-  struct rte_udp_hdr* udp_h =
-      (struct rte_udp_hdr*)((char*)ip_h + sizeof(struct rte_ipv4_hdr));
-  udp_h->src_port = rte_cpu_to_be_16(this->cfg_->BsServerPort() + tid);
-  udp_h->dst_port = rte_cpu_to_be_16(this->cfg_->BsRruPort() + tid);
-
-  tx_bufs[0]->pkt_len = this->cfg_->DlPacketLength() + kPayloadOffset;
-  tx_bufs[0]->data_len = this->cfg_->DlPacketLength() + kPayloadOffset;
-  char* payload = (char*)eth_hdr + kPayloadOffset;
-  DpdkTransport::FastMemcpy(payload, (char*)pkt, this->cfg_->DlPacketLength());
-
-  // Send data (one OFDM symbol)
-  size_t nb_tx_new = rte_eth_tx_burst(0, tid, tx_bufs, 1);
-  if (unlikely(nb_tx_new != 1)) {
-    std::printf("rte_eth_tx_burst() failed\n");
-    throw std::runtime_error("PacketTxRxDpdk: rte_eth_tx_burst() failed");
-  }
-  RtAssert(
-      message_queue_->enqueue(*rx_ptoks_[tid],
-                              EventData(EventType::kPacketTX, event.tags_[0])),
-      "Socket message enqueue failed\n");
-  return 1;
+bool PacketTxRxDpdk::CreateWorker(size_t tid, size_t interface_count,
+                                  size_t interface_offset,
+                                  size_t* rx_frame_start,
+                                  std::vector<RxPacket>& rx_memory,
+                                  std::byte* const tx_memory) {
+  RtAssert(false, "This function is not supported in DPDK mode\n") l
 }
--- src/agora/txrx/packet_txrx_dpdk.h
+++ src/agora/txrx/packet_txrx_dpdk.h
@@ -12,6 +12,7 @@ static_assert(false, "Packet tx rx dpdk defined but DPDK is not enabled");
 #endif
 
 #include "dpdk_transport.h"
+#include "packet_txrx.h"
 
 //Removed support for copy free dpdk memory due to slowdown issue.
 //#define USE_DPDK_MEMORY
@@ -40,29 +41,16 @@ class DPDKRxPacket : public RxPacket {
 #endif  // defined(USE_DPDK_MEMORY)
 
 /**
- * @brief Implementations of this class provide packet I/O for Agora.
- *
- * In the vanilla mode, this class provides socket or DPDK-based packet I/O to
- * Agora (running on the base station server or client) for communicating
- * with simulated peers.
- *
- * In the "Argos" mode, this class provides SoapySDR-based communication for
- * Agora (running on the base station server or client) for communicating
- * with real wireless hardware peers (antenna hubs for the server, UE devices
- * for the client).
+ * @brief Implementations of this class provide packet I/O for Agora using dpdk accelerations.
  */
-class PacketTxRxDpdk {
+class PacketTxRxDpdk : public PacketTxRx {
  public:
-  static const int kMaxSocketNum = 10;  // Max number of socket threads allowed
-
-  explicit PacketTxRxDpdk(Config* cfg, size_t in_core_offset = 1);
-
   PacketTxRxDpdk(Config* cfg, size_t core_offset,
                  moodycamel::ConcurrentQueue<EventData>* queue_message,
                  moodycamel::ConcurrentQueue<EventData>* queue_task,
                  moodycamel::ProducerToken** rx_ptoks,
                  moodycamel::ProducerToken** tx_ptoks);
-  ~PacketTxRxDpdk();
+  ~PacketTxRxDpdk() final override;
 
   /**
    * @brief Start the network I/O threads
@@ -74,30 +62,17 @@ class PacketTxRxDpdk {
    * otherwise
    */
   bool StartTxRx(Table<complex_float>& calib_dl_buffer_,
-                 Table<complex_float>& calib_ul_buffer_);
+                 Table<complex_float>& calib_ul_buffer_) final override;
 
  private:
+  explicit PacketTxRxDpdk(Config* cfg, size_t core_offset);
   void DoTxRx(size_t tid);  // The thread function for thread [tid]
 
   // At thread [tid], receive packets from the NIC and enqueue them to the
   // master thread
-  uint16_t DpdkRecv(int tid, uint16_t port_id, uint16_t queue_id,
+  uint16_t DpdkRecv(size_t tid, uint16_t port_id, uint16_t queue_id,
                     size_t& prev_frame_id, size_t& rx_slot);
-  int DequeueSend(int tid);
-
-  // The network I/O threads run on cores
-  // {core_offset, ..., core_offset + socket_thread_num - 1}
-  const size_t core_offset_;
-  const size_t num_interfaces_;
-  const size_t num_worker_threads_;
-
-  moodycamel::ConcurrentQueue<EventData>* event_notify_q_;
-  moodycamel::ConcurrentQueue<EventData>* tx_pending_q_;
-
-  //Producer tokens for posting messages to event_notify_q_
-  moodycamel::ProducerToken** notify_producer_tokens_;
-  //Producers assigned to tx_pending_q (used for retrieving messages)
-  moodycamel::ProducerToken** tx_producer_tokens_;
+  size_t DequeueSend(int tid);
 
   //const size_t ant_per_cell_;
   uint32_t bs_rru_addr_;     // IPv4 address of the simulator sender
@@ -107,10 +82,8 @@ class PacketTxRxDpdk {
   // Dimension 1: socket_thread
   // Dimension 2: rx_packet
 #if defined(USE_DPDK_MEMORY)
-  std::vector<std::vector<DPDKRxPacket>> rx_packets_;
-#else
-  std::vector<std::vector<RxPacket>> rx_packets_;
-#endif  // defined(USE_DPDK_MEMORY)
+  std::vector<std::vector<DPDKRxPacket>> rx_packets_dpdk_;
+#endif
 };
 
 #endif  // PACKETTXRX_DPDK_H_
